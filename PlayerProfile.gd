593805d0d06d5e1b172effe83711319dfde4fa6d
cseth1 <147735639+cseth1@users.noreply.github.com>
2024-10-22 15:57:50 -0500

    Update PlayerProfile.gd
    Track Player Progress in PlayerProfile.gd:

--- PlayerProfile.gd
+++ PlayerProfile.gd
@@ -1,11 +1,16 @@
+# PlayerProfile.gd
 extends Node
 
 var level = 1
 var xp = 0
+var total_xp = 0
+var tasks_completed = 0
 var xp_to_next_level = 100
 
 func add_xp(amount):
     xp += amount
+    total_xp += amount
+    AchievementManager.check_achievements()
     if xp >= xp_to_next_level:
         level_up()
 
@@ -13,3 +18,4 @@ func level_up():
     level += 1
     xp -= xp_to_next_level
     xp_to_next_level = int(xp_to_next_level * 1.5)
+    AchievementManager.check_achievements()
